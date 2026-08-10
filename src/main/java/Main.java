cb66d635aa5b29ad386b02d381b2d38911423ee8
Nischal Baniya <baniyanisal12@gmail.com>
2026-08-10 12:41:56 -0400

    data structure related with array done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,8 @@
 import custompackages.Information;
+import datastructure.ArrayConcept;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello Nischal");
-    System.out.println("This is java programming");
-    sum();
-
-    System.out.println("understanding concept for args in real----");
-
-    System.out.println("First arguement pass through terminal---" + args);
-
-    Calculator.add(2, 3);
-    Information.greetings("nischal dev");
-  }
-
-  public static void sum() {
-    System.out.println("Ths is sum function");
-
-    for (int i = 1; i < 19; i++) {
-      System.out.println("Loop runs for " + i + "times");
-    }
+    ArrayConcept.concept();
   }
 }
