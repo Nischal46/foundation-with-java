723aa41d8e5059bbdaeaf529ab6f8aabe66be085
Nischal Baniya <baniyanisal12@gmail.com>
2026-08-12 04:33:00 -0400

    solve array related question and then use tostring as override decorator to resolve string rather than memory address in response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,21 @@
 import datastructure.ArrayConcept;
-import exercises.arrayrelated.AlternateinArray;
+import exercises.arrayrelated.ArrayrelatedQuestions;
 
 public class Main {
   public static void main(String[] args) {
     // ArrayConcept.concept();
-    AlternateinArray.init();
-    AlternateinArray.searchelement(42);
-    AlternateinArray.findlargestelement();
-    AlternateinArray.checkarraysortedornot();
+    // AlternateinArray.init();
+    // AlternateinArray.searchelement(42);
+    // AlternateinArray.findlargestelement();
+    // AlternateinArray.checkarraysortedornot();
+
+    // finite array
+    int[] arr = new int[] { 1, 2, 4, 5, 3, 6 };
+
+    ArrayrelatedQuestions result = new ArrayrelatedQuestions(arr);
+    System.out.println("Logging of the constructor fn--" + result.isinitialized);
+    result.methodone();
+
+    ArrayrelatedQuestions.ArrayasObject();
   }
 }
